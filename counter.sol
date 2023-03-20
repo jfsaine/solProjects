3f49d2d6000c506b4d8d9a8a1734c528ecbb2b26
jfsaine <jfsaine@gmail.com>
2023-03-20 08:17:43 -0300

    Code Simplification

--- counter.sol
+++ counter.sol
@@ -1,19 +1,30 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
+
+// contract Counter {
+//     uint count;
+
+//     constructor() public {
+//         count = 0;
+//     }
+
+//     function getCount() public view returns(uint) {
+//         return count;
+//     }
+
+//     function incrementCount() public {
+//         count = count + 1;
+//     }
+// }
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Counter {
-    uint count;
-
-    constructor() public {
-        count = 0;
-    }
-
-    function getCount() public view returns(uint) {
-        return count;
-    }
+    uint public count = 0;
 
     function incrementCount() public {
-        count = count + 1;
+        count++;
     }
 }
 
